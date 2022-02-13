8157690669a454b6f36472c3db7716bcfce9f4fd
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2022-02-12 21:11:45 -0300

    chore: initial commit

---
+++ src/main/java/com/springur/demo/SpringUpRunningApplication.java
@@ -0,0 +1,13 @@
+package com.springur.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUpRunningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUpRunningApplication.class, args);
+    }
+
+}
