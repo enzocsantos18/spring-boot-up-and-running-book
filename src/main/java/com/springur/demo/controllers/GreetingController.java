886a02e7a7536bb1ae9fafdfa74ccc6c0a3efd51
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2022-02-13 10:31:37 -0300

    feature: adding application properties interaction

---
+++ src/main/java/com/springur/demo/controllers/GreetingController.java
@@ -0,0 +1,19 @@
+package com.springur.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    @Value("${greeting-name: Mirage}")
+    private String name;
+
+    @GetMapping
+    String getGreeting() {
+        return "Welcome " + name + " :)";
+    }
+}
