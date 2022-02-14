257f31b5a7aaae43a91f57670c9027e0f18adf1b
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2022-02-13 21:20:15 -0300

    feaure: adding verified configuration properties

---
+++ src/main/java/com/springur/demo/controllers/DroidController.java
@@ -0,0 +1,23 @@
+package com.springur.demo.controllers;
+
+import com.springur.demo.config.Droid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/droid")
+public class DroidController {
+
+    private Droid droid;
+
+    public DroidController(Droid droid) {
+        this.droid = droid;
+    }
+
+    @GetMapping
+    Droid getDroid() {
+        return droid;
+    }
+
+}
